15df0fc5d2da34febf9754fe56f3e5c13c06c631
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-04-28 12:42:52 -0500

    initial commit - created base files and built a plan

---
+++ client.js
@@ -0,0 +1,10 @@
+/* Weekend Salary Calc by Steve - Planning
+** Plan **
+** 1 - create files 
+** 2 - build boilerplates and link all files
+** 3 - create basic html elements
+** 4 - style html elements
+** 5 - add form handling in js
+** 6 - tbd
+** x - add extra functionality/styling
+*/
