6684e050420c1d18357c9aa118a041a74c1e8c8d
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-05-05 20:42:41 -0500

    added a server to the project

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = 8000;
+const app = express();
+
+app.use(express.static('public/'));
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
